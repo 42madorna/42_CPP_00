5e225611f6011374f821685cccb132cd32326383
Miguel Ángel Adorna Ruiz (madorna-) <madorna-@student.42madrid.com>
2022-01-27 20:54:05 +0100

    Written Account class functions

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,9 +6,37 @@
 /*   By: madorna- <madorna-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 05:43:40 by madorna-          #+#    #+#             */
-/*   Updated: 2022/01/12 05:43:44 by madorna-         ###   ########.fr       */
+/*   Updated: 2022/01/27 20:53:11 by madorna-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "Account.hpp"
 
+void
+	Account::makeDeposit(int deposit)
+{
+}
+
+bool
+	Account::makeWithdrawal(int withdrawal)
+{
+}
+
+void
+	Account::displayAccountsInfos(void)
+{
+}
+
+Account::Account(int initial_deposit)
+{
+}
+
+Account::~Account(void)
+{
+}
+
+void
+	Account::displayStatus(void) const
+{
+}
